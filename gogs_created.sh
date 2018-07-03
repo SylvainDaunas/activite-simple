4f27598e74b65ebabac1ba310012ef70d87b8567
daunas <sylvain.daunas@enedis-grdf.fr>
2018-07-03 11:51:52 +0200

    Creation scipt + declaration variable

---
+++ gogs_created.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#---------------------------------------------------------------------#
+#@(#) SCRIPT : gogs_created.sh
+#---------------------------------------------------------------------#
+#@(#) Fonction             : Script permettant la creation auto depot gogs
+#@(#) Auteur               : DEA Bourges
+#@(#) Parametres d'entree  :
+#@(#) Parametres de sortie :
+#@(#) Retour               : 0=Ok, Sup a 0=Erreur
+#@(#) Scripts appeles      : Script du PDIS modifie pour nos besoins
+#---------------------------------------------------------------------#
+#	1.00	SDS + RLL 	27/04/18	 Version initiale
+#---------------------------------------------------------------------#
+
+#---------------------------------------------------------------------#
+# Variables
+#---------------------------------------------------------------------#
+#NNIADM=""
+#NNIRW=""
+#NNIR=""
+#NNILIST="$NNIADM $NNIRW $NNIR"
+#APPLICATION=""
+#NAME_DEPOT="dep_$APPLICATION"
+#DESCRIPTION_DEPOT="Dépôt pour l'application $APPLICATION"
+clear
